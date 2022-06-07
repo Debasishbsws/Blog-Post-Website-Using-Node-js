3938b4562427c82593c311cf38110361eb38c8f8
Debasish Biswas <debasishbsws.abc@gmail.com>
2022-06-07 05:52:44 +0000

    Mongo DataBase connected

--- app.js
+++ app.js
@@ -3,27 +3,48 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 const _ = require("lodash");
+
+const mongooseUrl = "mongodb://localhost:27017"; // this is for the localhost mongoDB
+
+mongoose.connect(`${mongooseUrl}/blogPostDB`);
+
+const postSchema = new mongoose.Schema({
+  postTitle: String,
+  postContent: String
+});
+const Post = mongoose.model("Post", postSchema);
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-let posts = [];
+
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
+
 app.use(express.static("public"));
 
+// get & post routes ---------------------------------------------------------
+
 app.get("/", function (req, res) {
-  res.render("home", {
-    HomeContent: homeStartingContent,
-    posts: posts,
+  // let posts;
+  Post.find({}, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("home", {
+        HomeContent: homeStartingContent,
+        posts: data,
+      });
+    }
   });
+
 });
 
 app.get("/about", (req, res) => {
@@ -40,30 +61,37 @@ app.get("/compose", (req, res) => {
 
 app.post("/compose", (req, res) => {
 
-  const post = {
-    title: req.body.postTitle,
-    content: req.body.postContent,
-  };
+  const newPost = new Post({
+    postTitle: req.body.postTitle,
+    postContent: req.body.postContent
+  });
 
-  posts.push(post);
-
-  res.redirect("/");
-});
-
-
-app.get("/posts/:postName", (req, res) => {
-  // console.log(req.params.id);
-  posts.forEach((post) => {
-    if (_.lowerCase(post.title) === _.lowerCase(req.params.postName)) {
-      res.render("post", { postTitle: post.title, postContent: post.content });
+  newPost.save((err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect("/");
     }
   });
 
 });
 
 
+app.get("/posts/:postId", (req, res) => {
+  // console.log(req.params.postId);
+
+  Post.findById(req.params.postId, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("post", { postTitle: data.postTitle, postContent: data.postContent });
+    }
+  });
+});
 
 
+
+// app listen post-------------------------------------
 app.listen(8080, function () {
   console.log("Server started on port 8080");
 });
